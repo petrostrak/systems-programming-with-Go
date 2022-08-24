f31265c1ded1e12989228f6aaa04557b50993522
petrostrak <pit.trak@gmail.com>
2022-08-24 17:09:45 +0300

    Setting a timeout

---
+++ 08-Writing-Web-Applications-in-Go/timeout-getURL/timeoutHTTP.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+var (
+	timeout = time.Duration(time.Second)
+)
+
+func timeOut(network, host string) (net.Conn, error) {
+	conn, err := net.DialTimeout(network, host, timeout)
+	if err != nil {
+		return nil, err
+	}
+
+	conn.SetDeadline(time.Now().Add(timeout))
+
+	return conn, nil
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Printf("Usage: %s URL\n", filepath.Base(os.Args[0]))
+		os.Exit(1)
+	}
+	URL := os.Args[1]
+
+	t := http.Transport{
+		Dial: timeOut,
+	}
+
+	client := http.Client{
+		Transport: &t,
+	}
+
+	data, err := client.Get(URL)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	} else {
+		defer data.Body.Close()
+		_, err := io.Copy(os.Stdout, data.Body)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+}
