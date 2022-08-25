10cda2a34072527a48463e8ff12ce2a85853279f
petrostrak <pit.trak@gmail.com>
2022-08-25 19:32:19 +0300

    A Unix socket server

---
+++ 09-Network-Programming/unix-socket-server/socketServer.go
@@ -0,0 +1,52 @@
+// The Unix socket server will act as an Echo server,
+// which means that it will send the received message
+// back to the client.
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func echoServer(c net.Conn) {
+	for {
+		buf := make([]byte, 1024)
+		nr, err := c.Read(buf)
+		if err != nil {
+			return
+		}
+
+		data := buf[0:nr]
+		fmt.Printf("->: %v\n", string(data))
+		_, err = c.Write(data)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
+
+func main() {
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("Please provide a socket file.")
+		os.Exit(100)
+	}
+	socketFile := arguments[1]
+
+	l, err := net.Listen("unix", socketFile)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	for {
+		fd, err := l.Accept()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		go echoServer(fd)
+	}
+}
