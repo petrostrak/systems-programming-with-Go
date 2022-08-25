0b25decb177c2cd8a5f1d396f0fe807fe02c2111
petrostrak <pit.trak@gmail.com>
2022-08-25 10:06:29 +0300

    Saving JSON data

---
+++ 08-Writing-Web-Applications-in-Go/save-JSON-data/writeJSON.go
@@ -0,0 +1,57 @@
+// Convert regular data into JSON format in order to
+// send it over a network connection.
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Record struct {
+	Name    string
+	Surname string
+	Tel     []Telephone
+}
+
+type Telephone struct {
+	Mobile bool
+	Number string
+}
+
+func toJSON(filename string, key any) {
+	out, err := os.Create(filename)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer out.Close()
+
+	encodeJSON := json.NewEncoder(out)
+	err = encodeJSON.Encode(key)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+// go run writeJSON.go record
+func main() {
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("Please provide a filename!")
+		os.Exit(100)
+	}
+	filename := arguments[1]
+
+	myRecord := Record{
+		Name:    "Petros",
+		Surname: "Trak",
+		Tel: []Telephone{
+			{Mobile: true, Number: "1234-abcd"},
+			{Mobile: false, Number: "abcc-567"},
+		},
+	}
+
+	toJSON(filename, myRecord)
+}
